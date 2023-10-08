6dd1c503f9e66ffbb8539fb4a27b37ea11510096
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-10-08 14:33:38 +0530

    component replaced with elements as we using v6 of react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
-import HomePage from './pages/HomePage';
 import CoinPage from './pages/CoinPage';
+import Homepage from './pages/HomePage';
+import { makeStyles } from '@material-ui/core';
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App:{
+      backgroundColor: '#011936',
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
-        <Route path="/" component={HomePage} />
-        <Route path='coins/id:' component={CoinPage}  />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path='/coins/:id' element={<CoinPage />}  />
+        </Routes>
       </div>
     </BrowserRouter>
   );
